6bbda9e58389f51f078c2d1740f164ae6825b6dc
rick-five <165170199+rick-five@users.noreply.github.com>
2025-08-13 21:28:41 +0800

    fix-build-script-format-2

--- minigu/python/build.rs
+++ minigu/python/build.rs
@@ -3,7 +3,7 @@ use std::env;
 fn main() {
     // Enable PyO3's automatic linking feature
     pyo3_build_config::use_pyo3_cfgs();
-    
+
     // Special handling for macOS
     if env::var("TARGET").is_ok_and(|target| target.contains("apple")) {
         // Try to automatically find and link Python
