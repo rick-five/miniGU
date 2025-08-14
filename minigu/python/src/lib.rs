7a1e43c2b31faa910cc32754e59d44e3a30b2b2e
rick-five <165170199+rick-five@users.noreply.github.com>
2025-08-14 16:27:29 +0800

    "fix"

--- minigu/python/src/lib.rs
+++ minigu/python/src/lib.rs
@@ -5,7 +5,7 @@
 use arrow::array::*;
 use arrow::datatypes::DataType;
 use pyo3::prelude::*;
-use pyo3::types::{PyDict, PyList, PyString};
+use pyo3::types::{PyDict, PyList, PyString, PyModule};
 
 use minigu::database::{Database, DatabaseConfig};
 use minigu::session::Session;
