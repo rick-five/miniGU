c7990bd50cd41e44e894d566f2f420b1694f36b7
rick-five <165170199+rick-five@users.noreply.github.com>
2025-08-19 20:46:57 +0800

    Update __init__.py

--- minigu/__init__.py
+++ minigu/__init__.py
@@ -1,4 +1,4 @@
-# 主模块初始化文件
+
 from .python.minigu import connect, async_connect, MiniGU, AsyncMiniGU, QueryResult, Node, Edge, Path, MiniGUError, ConnectionError, QueryError, DataError, GraphError, HAS_RUST_BINDINGS
 
 __all__ = ['connect', 'async_connect', 'MiniGU', 'AsyncMiniGU', 'QueryResult', 'Node', 'Edge', 'Path', 'MiniGUError', 'ConnectionError', 'QueryError', 'DataError', 'GraphError', 'HAS_RUST_BINDINGS']
