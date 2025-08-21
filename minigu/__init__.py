85b594724367e34ae42e66167b4314040eb8f82b
rick-five <165170199+rick-five@users.noreply.github.com>
2025-08-21 16:19:59 +0800

    Delete minigu/__init__.py

--- minigu/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from .python.minigu import connect, async_connect, MiniGU, AsyncMiniGU, QueryResult, Node, Edge, Path, MiniGUError, ConnectionError, QueryError, DataError, GraphError, HAS_RUST_BINDINGS
-
-__all__ = ['connect', 'async_connect', 'MiniGU', 'AsyncMiniGU', 'QueryResult', 'Node', 'Edge', 'Path', 'MiniGUError', 'ConnectionError', 'QueryError', 'DataError', 'GraphError', 'HAS_RUST_BINDINGS']
