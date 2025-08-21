4dc2d30af463ac0ea9634f664ee420c82c5ae74e
rick-five <165170199+rick-five@users.noreply.github.com>
2025-08-21 16:35:44 +0800

    Delete minigu/minigu directory

--- minigu/minigu/__init__.py
+++
@@ -1,6 +0,0 @@
-from ..python.minigu import connect, MiniGU, QueryResult, MiniGUError
-
-__all__ = ['connect', 'MiniGU', 'QueryResult', 'MiniGUError']
-from .minigu import connect, MiniGU, QueryResult, MiniGUError
-
-__all__ = ['connect', 'MiniGU', 'QueryResult', 'MiniGUError']
